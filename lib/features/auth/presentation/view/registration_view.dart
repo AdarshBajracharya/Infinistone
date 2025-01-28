be3e5f97cab397cfbe124ea83a8de43ac36b06d0
AdarshBajracharya <abajracharya55@gmail.com>
2025-01-28 21:18:03 +0545

    profile picture

--- lib/features/auth/presentation/view/registration_view.dart
+++ lib/features/auth/presentation/view/registration_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:infinistone/features/auth/presentation/view_model/signup/register_bloc.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class RegistrationView extends StatefulWidget {
   const RegistrationView({super.key});
@@ -23,49 +24,25 @@ class _RegistrationViewState extends State<RegistrationView> {
   final TextEditingController confirmPasswordController =
       TextEditingController();
 
-  File? _profileImage;
+  checkCameraPermission() async {
+    if (await Permission.camera.request().isRestricted ||
+        await Permission.camera.request().isDenied) {
+      await Permission.camera.request();
+    }
+  }
 
-  Future<void> _pickImage() async {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return SafeArea(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                leading: const Icon(Icons.camera_alt, color: Colors.black),
-                title: const Text('Take a photo'),
-                onTap: () async {
-                  Navigator.pop(context);
-                  final pickedFile =
-                      await ImagePicker().pickImage(source: ImageSource.camera);
-                  if (pickedFile != null) {
-                    setState(() {
-                      _profileImage = File(pickedFile.path);
-                    });
-                  }
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.photo, color: Colors.black),
-                title: const Text('Choose from gallery'),
-                onTap: () async {
-                  Navigator.pop(context);
-                  final pickedFile = await ImagePicker()
-                      .pickImage(source: ImageSource.gallery);
-                  if (pickedFile != null) {
-                    setState(() {
-                      _profileImage = File(pickedFile.path);
-                    });
-                  }
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
+  File? _img;
+  Future _browseImage(ImageSource imageSource) async {
+    try {
+      final image = await ImagePicker().pickImage(source: imageSource);
+      if (image != null) {
+        setState(() {
+          _img = File(image.path);
+        });
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   Widget _buildTextField({
@@ -76,10 +53,11 @@ class _RegistrationViewState extends State<RegistrationView> {
   }) {
     return TextFormField(
       controller: controller,
+      style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
         labelText: label,
-        labelStyle: const TextStyle(color: Colors.white70),
-        prefixIcon: Icon(icon, color: Colors.white70),
+        labelStyle: const TextStyle(color: Colors.white),
+        prefixIcon: Icon(icon, color: Colors.white),
         filled: true,
         fillColor: Colors.white.withOpacity(0.2),
         enabledBorder: OutlineInputBorder(
@@ -113,150 +91,209 @@ class _RegistrationViewState extends State<RegistrationView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
+      resizeToAvoidBottomInset: true,
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(
-                'assets/images/background_auth.jpg'), // Your background image
+            image: AssetImage('assets/images/reg.jpg'),
             fit: BoxFit.cover,
           ),
         ),
         height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center the content
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.arrow_back, color: Colors.white),
-                  onPressed: () {
-                    Navigator.pop(context); // Navigate back to the login screen
-                  },
-                ),
-              ],
-            ),
-            Padding(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: SingleChildScrollView(
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 30),
-                      const Text(
-                        "Welcome to Infinistone",
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          icon:
+                              const Icon(Icons.arrow_back, color: Colors.white),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    const Text(
+                      "Hello.",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    const SizedBox(height: 30),
+                    InkWell(
+                      onTap: () {
+                        showModalBottomSheet(
+                          backgroundColor:
+                              Colors.black, // Set background to black
+                          context: context,
+                          isScrollControlled: true,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(20),
+                            ),
+                          ),
+                          builder: (context) => Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                ElevatedButton.icon(
+                                  onPressed: () {
+                                    checkCameraPermission();
+                                    _browseImage(ImageSource.camera);
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(Icons.camera,
+                                      color: Colors.white),
+                                  label: const Text(
+                                    'Camera',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.transparent,
+                                    disabledForegroundColor:
+                                        Colors.white.withOpacity(0.38),
+                                    disabledBackgroundColor:
+                                        Colors.white.withOpacity(0.12),
+                                    side: const BorderSide(color: Colors.white),
+                                  ),
+                                ),
+                                ElevatedButton.icon(
+                                  onPressed: () {
+                                    _browseImage(ImageSource.gallery);
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(Icons.image,
+                                      color: Colors.white),
+                                  label: const Text(
+                                    'Gallery',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.transparent,
+                                    disabledForegroundColor:
+                                        Colors.white.withOpacity(0.38),
+                                    disabledBackgroundColor:
+                                        Colors.white.withOpacity(0.12),
+                                    side: const BorderSide(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      child: SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: _img != null
+                              ? FileImage(_img!)
+                              : const AssetImage('assets/images/profile.png')
+                                  as ImageProvider,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    _buildTextField(
+                      controller: firstNameController,
+                      label: "First Name",
+                      icon: Icons.person,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                      controller: lastNameController,
+                      label: "Last Name",
+                      icon: Icons.person,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                      controller: emailController,
+                      label: "Email",
+                      icon: Icons.email,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                      controller: phoneController,
+                      label: "Phone Number",
+                      icon: Icons.phone,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                      controller: addressController,
+                      label: "Address",
+                      icon: Icons.home,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                      controller: passwordController,
+                      label: "Password",
+                      icon: Icons.lock,
+                      isPassword: true,
+                    ),
+                    const SizedBox(height: 16),
+                    _buildTextField(
+                      controller: confirmPasswordController,
+                      label: "Confirm Password",
+                      icon: Icons.lock_outline,
+                      isPassword: true,
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState?.validate() ?? false) {
+                          context.read<RegisterBloc>().add(
+                                RegisterUser(
+                                  context: context,
+                                  fName: firstNameController.text,
+                                  lName: lastNameController.text,
+                                  phone: phoneController.text,
+                                  email: emailController.text,
+                                  address: addressController.text,
+                                  password: passwordController.text,
+                                ),
+                              );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 18, horizontal: 64),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        elevation: 6,
+                        shadowColor: Colors.grey.withOpacity(0.2),
+                      ),
+                      child: const Text(
+                        "Register",
                         style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
+                          color: Colors.black,
+                          fontSize: 18,
                           fontWeight: FontWeight.bold,
                         ),
-                        textAlign: TextAlign.center,
                       ),
-                      const SizedBox(height: 30),
-                      GestureDetector(
-                        onTap: _pickImage,
-                        child: CircleAvatar(
-                          radius: 60,
-                          backgroundColor: Colors.grey[200],
-                          backgroundImage: _profileImage != null
-                              ? FileImage(_profileImage!)
-                              : null,
-                          child: _profileImage == null
-                              ? const Icon(
-                                  Icons.camera_alt,
-                                  size: 40,
-                                  color: Colors.black45,
-                                )
-                              : null,
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      _buildTextField(
-                        controller: firstNameController,
-                        label: "First Name",
-                        icon: Icons.person,
-                      ),
-                      const SizedBox(height: 16),
-                      _buildTextField(
-                        controller: lastNameController,
-                        label: "Last Name",
-                        icon: Icons.person,
-                      ),
-                      const SizedBox(height: 16),
-                      _buildTextField(
-                        controller: emailController,
-                        label: "Email",
-                        icon: Icons.email,
-                      ),
-                      const SizedBox(height: 16),
-                      _buildTextField(
-                        controller: phoneController,
-                        label: "Phone Number",
-                        icon: Icons.phone,
-                      ),
-                      const SizedBox(height: 16),
-                      _buildTextField(
-                        controller: addressController,
-                        label: "Address",
-                        icon: Icons.home,
-                      ),
-                      const SizedBox(height: 16),
-                      _buildTextField(
-                        controller: passwordController,
-                        label: "Password",
-                        icon: Icons.lock,
-                        isPassword: true,
-                      ),
-                      const SizedBox(height: 16),
-                      _buildTextField(
-                        controller: confirmPasswordController,
-                        label: "Confirm Password",
-                        icon: Icons.lock_outline,
-                        isPassword: true,
-                      ),
-                      const SizedBox(height: 30),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (formKey.currentState?.validate() ?? false) {
-                            context.read<RegisterBloc>().add(
-                                  RegisterUser(
-                                    context: context,
-                                    fName: firstNameController.text,
-                                    lName: lastNameController.text,
-                                    phone: phoneController.text,
-                                    email: emailController.text,
-                                    address: addressController.text,
-                                    password: passwordController.text,
-                                  ),
-                                );
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 18, horizontal: 64),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          elevation: 6,
-                          shadowColor: Colors.grey.withOpacity(0.2),
-                        ),
-                        child: const Text(
-                          "Register",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 30),
+                    const SizedBox(height: 30),
+                  ],
                 ),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
