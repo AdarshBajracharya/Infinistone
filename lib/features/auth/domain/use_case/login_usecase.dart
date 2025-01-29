88f9390ceec71a780fd109064a44ca26269e3fbe
AdarshBajracharya <abajracharya55@gmail.com>
2025-01-29 21:37:01 +0545

    login and shared prefs

--- lib/features/auth/domain/use_case/login_usecase.dart
+++ lib/features/auth/domain/use_case/login_usecase.dart
@@ -1,35 +1,47 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:infinistone/app/shared_prefs/token_shared_prefs.dart';
 import 'package:infinistone/app/usecase/usecase.dart';
 import 'package:infinistone/core/error/failure.dart';
 import 'package:infinistone/features/auth/domain/repository/auth_repository.dart';
 
 class LoginParams extends Equatable {
-  final String username;
+  final String email;
   final String password;
 
   const LoginParams({
-    required this.username,
+    required this.email,
     required this.password,
   });
 
   // Initial Constructor
   const LoginParams.initial()
-      : username = '',
+      : email = '',
         password = '';
 
   @override
-  List<Object> get props => [username, password];
+  List<Object> get props => [email, password];
 }
 
 class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
   final IAuthRepository repository;
+  final TokenSharedPrefs tokenSharedPrefs;
 
-  LoginUseCase(this.repository);
+  LoginUseCase(this.repository, this.tokenSharedPrefs);
 
   @override
   Future<Either<Failure, String>> call(LoginParams params) {
-    // IF api then store token in shared preferences
-    return repository.loginUser(params.username, params.password);
+    return repository.loginUser(params.email, params.password).then((value) {
+      return value.fold(
+        (failure) => Left(failure),
+        (token) {
+          tokenSharedPrefs.saveToken(token);
+          tokenSharedPrefs.getToken().then((value) {
+            print(value);
+          });
+          return Right(token);
+        },
+      );
+    });
   }
 }
